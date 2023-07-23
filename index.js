dc85865d2753832d04f904a079a6f5c24a8e7b91
ZwierQ <81569969+ZwierQ@users.noreply.github.com>
2023-07-23 11:10:15 +0200

    refactor: extract similar code to one function
    Logic for changing css classes in passwords validation was almost
    the same in both cases. Extracted it to single function to prevent code
    repetitions.

--- index.js
+++ index.js
@@ -21,33 +21,34 @@ function validatePasswords(password1, password2) {
     return [errorMessage, isValid]
 }
 
+function toggleValidity() {
+    password.classList.toggle("error")
+    password.classList.toggle("correct")
+
+    confirmPassword.classList.toggle("error")
+    confirmPassword.classList.toggle("correct")
+
+    
+    errorParagraphs.forEach((elem) => {
+        elem.classList.toggle("invisible")
+    })
+}
+
+function setError(message) {
+    errorParagraphs.forEach((elem) => {
+        elem.textContent = message
+    })
+}
+
 passwordInputs.forEach(function(elem) {
     elem.addEventListener("input", () => {
         [errorMessage ,isValid] = validatePasswords(password.value, confirmPassword.value)
         
         if (isValid) {
-            password.classList.remove("error")
-            password.classList.add("correct")
-            
-            confirmPassword.classList.remove("error")
-            confirmPassword.classList.add("correct")
-            
-            errorParagraphs.forEach((elem) => {
-                elem.textContent = errorMessage
-                elem.classList.add("invisible")
-            })
-            
+            !!password.classList.contains("error") && toggleValidity() 
         } else {
-            password.classList.remove("correct")
-            password.classList.add("error")
-    
-            confirmPassword.classList.remove("correct")
-            confirmPassword.classList.add("error")
-            
-            errorParagraphs.forEach((elem) => {
-                elem.textContent = errorMessage
-                elem.classList.remove("invisible")
-            })
+            !!password.classList.contains("correct") && toggleValidity()
         }
+        setError(errorMessage)
     })
 })
