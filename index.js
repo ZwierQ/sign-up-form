e1e0741cb0b0579ae371f0ae93d584c8e34029af
ZwierQ <81569969+ZwierQ@users.noreply.github.com>
2023-07-24 10:59:30 +0200

    style: add new font to page

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const errorParagraphs = document.querySelectorAll(".error-message")
 function validatePasswords(password1, password2) {
     let errorMessage = "";
     let isValid = false
+    
     if (password1.length < 8) {
-        isValid = false
         errorMessage = "Password is too short"
     } else if (password1 !== password2) {
-        isValid = false
         errorMessage = "Passwords doesn't match"
     } else if (password1 === password2 && password1.length >= 8) {
         isValid = true
@@ -22,12 +21,10 @@ function validatePasswords(password1, password2) {
 }
 
 function toggleValidity() {
-    password.classList.toggle("error")
-    password.classList.toggle("correct")
-
-    confirmPassword.classList.toggle("error")
-    confirmPassword.classList.toggle("correct")
-
+    passwordInputs.forEach((elem) => {
+        elem.classList.toggle("error")
+        elem.classList.toggle("correct")
+    })
     
     errorParagraphs.forEach((elem) => {
         elem.classList.toggle("invisible")
@@ -42,7 +39,7 @@ function setError(message) {
 
 passwordInputs.forEach(function(elem) {
     elem.addEventListener("input", () => {
-        [errorMessage ,isValid] = validatePasswords(password.value, confirmPassword.value)
+        [errorMessage, isValid] = validatePasswords(password.value, confirmPassword.value)
         
         if (isValid) {
             !!password.classList.contains("error") && toggleValidity() 
